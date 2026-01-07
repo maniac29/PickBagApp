9bb53c6d7bec3c58b131be940bcaeaa8c3c15eb7
Anirban Saha <69422729+maniac29@users.noreply.github.com>
2026-01-07 21:42:10 +0530

    admin panel and products added

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const productModel = require("../models/product-model")
 const router = express.Router();
 const isLoggedIn = require("../middlewares/isLoggedIn");
 
@@ -7,8 +8,9 @@ router.get("/", (req, res) => {
   res.render("index", { error });
 });
 
-router.get("/shop", isLoggedIn, function (req, res) {
-  res.render("shop");
+router.get("/shop", isLoggedIn, async function (req, res) {
+   let products = await productModel.find()
+  res.render("shop",{products});
 });
 
 module.exports = router;
